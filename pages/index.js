a6d68786454871491e879e4c61db9dbef3e8d326
gregsvieira <89167868+gregsvieira@users.noreply.github.com>
2022-01-26 12:00:31 -0300

    chat atualized

--- pages/index.js
+++ pages/index.js
@@ -4,36 +4,7 @@ import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-          align-items: center;
-          justifycontent: center;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
+
 
 function Titulo(props){
     console.log(props);
@@ -69,7 +40,7 @@ function Titulo(props){
 
 export default function PaginaInicial() {
     // const username = 'omariosouto';
-    const [username, setUsername] = React.useState('gregsvieira');
+    const [username, setUsername] = React.useState('');
     const roteamento = useRouter();
   
     return (
@@ -101,12 +72,15 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
-              onSubmit={function (infosDoEvento) {
+              onSubmit={function (infosDoEvento){
                 infosDoEvento.preventDefault();
                 console.log('Alguém submeteu o form');
                 roteamento.push('/chat');
-                // window.location.href = '/chat';
+
+                // window.location.href = '/chat'; JEITO ANTIGO
               }}
+           
+              
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -130,7 +104,7 @@ export default function PaginaInicial() {
                               }}
                           /> */}
               <TextField
-                value={username}
+                value = {username}
                 onChange={function (event) {
                   console.log('usuario digitou', event.target.value);
                   // Onde ta o valor?
